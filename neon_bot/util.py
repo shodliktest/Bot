80d86611d003873930c9a018b274a5734815ca90
shodliktest <shodlikfire@gmail.com>
2026-01-19 22:30:09 +0500

    Update util.py

--- neon_bot/util.py
+++ neon_bot/util.py
@@ -1,1 +1,15 @@
+# util.py
+import os
 
+def safe_remove(path: str):
+    try:
+        if os.path.exists(path):
+            os.remove(path)
+    except Exception:
+        pass
+
+async def limit_cache(cache: dict, limit: int = 1000, drop: int = 200):
+    if len(cache) > limit:
+        keys = list(cache.keys())[:drop]
+        for k in keys:
+            cache.pop(k, None)
