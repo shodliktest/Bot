ef96571fe0c21b4c2faff182d4a5d5c13ede0343
shodliktest <shodlikfire@gmail.com>
2026-01-21 09:25:11 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,21 +2,21 @@ import os
 import pytz
 from datetime import datetime
 
-# Konfiguratsiya
+# O'zbekiston vaqti
 UZ_TZ = pytz.timezone('Asia/Tashkent')
 
 def get_uz_time():
-    """O'zbekiston vaqti (YYYY.MM.DD HH:MM:SS)"""
+    """Hozirgi sanani qaytaradi: YYYY.MM.DD HH:MM:SS"""
     return datetime.now(UZ_TZ).strftime('%Y.%m.%d %H:%M:%S')
 
 def clean_text(text):
     """
-    Telegram HTML formati uchun matnni xavfsiz holatga keltirish.
-    Apostrofni (&#x27;) kodga aylantirmaslik uchun faqat asosiy belgilarni o'zgartiradi.
+    Telegram HTML rejimi uchun matnni tozalash.
+    DIQQAT: Apostrof (') va qo'shtirnoq (") o'zgartirilmaydi!
+    Faqat &, <, > belgilarini xavfsiz holatga keltiramiz.
     """
     if not text:
         return ""
-    # Telegram HTML rejimi uchun faqat bularni escape qilish shart
     return text.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
 
 def delete_temp_files(*file_paths):
@@ -29,7 +29,8 @@ def delete_temp_files(*file_paths):
                 pass
 
 def format_time_stamp(seconds):
-    """Whisper sekundlarini [MM:SS] ko'rinishiga o'tkazish"""
+    """Sekundlarni [MM:SS] formatiga o'tkazish"""
     minutes = int(seconds // 60)
     secs = int(seconds % 60)
     return f"[{minutes:02d}:{secs:02d}]"
+    
